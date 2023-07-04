5f261a66d28459fe2ad63cf08d1165341f1d7c43
David Kemp <19152940+davaya@users.noreply.github.com>
2023-07-03 21:00:03 -0400

    add usage description

---
+++ model/USING.md
@@ -0,0 +1,48 @@
+# Using the Serialization Playground
+
+## Structure
+The playground directory structure has three components:
+1. A directory of Python [template classes](generated), with one template for each type defined by the model.
+Each template lists just the properties of the type, with no property values.
+2. A directory of Python [example classes](../ex) with property values from a template class filled in,
+3. A directory of serialized examples for each proposed serialization format, e.g., [JSON1](../json1).
+
+### Template Classes
+The template classes are generated from the logical model:
+```
+class Sbom:
+    sbomType: SBOMType = None
+    context: str = None
+    element: Element = None
+    rootElement: Element = None
+    namespaces: NamespaceMap = None
+    imports: ExternalMap = None
+    spdxId: str = None
+    name: str = None
+    summary: str = None
+    description: str = None
+    comment: str = None
+    creationInfo: CreationInfo = None
+    verifiedUsing: IntegrityMethod = None
+    externalReference: ExternalReference = None
+    externalIdentifier: ExternalIdentifier = None
+    extension: Extension = None
+```
+
+### Example Logical Values
+One example for each of the use cases listed in the [README](README.md).
+Each example logical value is a copy of the template for that type with property values filled in.
+Many of the model types will not have a use case, and a few types will have several use cases / examples.
+Contributors may propose additional example logical values to accompany corresponding serialized data.
+
+These are the logical values to be serialized into various serialization formats.
+All required properties must have a value; optional properties may be left empty (Python None).
+```
+class Sbom:
+    sbomType: SBOMType = 'source'
+    context: str = None
+    ... TBSL ...
+```
+### Serialized Examples
+The contributor of a format shows how an example logical value is serialized and parsed
+to/from the serialized value for that use case.
--- model/make_types.py
+++ model/make_types.py
@@ -11,6 +11,12 @@ from graphlib import TopologicalSorter
 OUT_DIR = 'generated'
 MODEL_SNAPSHOT = os.path.join(OUT_DIR, 'modelTypes.json')
 
+typemap = {
+    'xsd:anyURI': 'str',
+    'xsd:integer': 'int',
+    'xsd:string': 'str'
+}
+
 
 def write_tools_class(model, mtypes, out):
     meta = mtypes[model]['Metadata']
@@ -23,23 +29,15 @@ def write_tools_class(model, mtypes, out):
     class_name = meta['name']
     parent_name = meta.get('SubclassOf', '')
     with open(os.path.join(pdir, meta['name']) + '.py', 'w') as fp:
-        fp.write(
-            f'# SPDX-FileCopyrightText: 2023 spdx contributors\n'
-            f'#\n'
-            f'# SPDX-License-Identifier: Apache-2.0\n'
-            f'from dataclasses import dataclass, field\n'
-            f'from spdx_tools.common.typing.dataclass_with_properties import dataclass_with_properties\n'
-            f'from spdx_tools.common.typing.type_checks import check_types_and_set_values\n'
-            f'from spdx_tools.spdx3.model import CreationInfo, Element\n'
-            f'\n\n'
-            f'@dataclass\n'
-            f'class {class_name}():\n'
-        )
         if meta['_category'] == 'Classes':
+            fp.write(f'class {class_name}:\n')
             for k, v in mtypes[model]['Properties'].items():
-                fp.write(
-                    f'    {k}: {v["type"]}\n'
-                )
+                ptype = typemap.get(v['type'], v['type'])
+                fp.write(f'    {k}: {ptype} = None\n')
+        elif meta['_category'] == 'Vocabularies':
+            fp.write(f'class {class_name}(Enum):\n')
+            for n, v in enumerate(mtypes[model]['Entries'], start=1):
+                fp.write(f'    {v} = {n}\n')
 
 
 def subclass(td):
@@ -50,18 +48,23 @@ def subclass(td):
 def build_td(tname, model_types):
     if td := model_types.get(tname, {}):
         if 'Properties' in td:
+            # Apply default property constraints
             for k, tdp in td['Properties'].items():
                 p = {'minCount': 0, 'maxCount': '*'}
                 p.update(tdp)
+                if p['maxCount'] != '*':
+                    assert int(p['minCount']) <= int(p['maxCount'])
                 td['Properties'][k] = p
+            # Propagate properties to subclasses
             if sd := model_types.get(subclass(td), {}):     # TODO: generate list types
                 for k, p in sd['Properties'].items():
                     if k in td['Properties'] and p != td['Properties'][k]:
                         tdp = td['Properties'][k]    # ensure restrictions
                         assert tdp['type'] == p['type']
+                        assert int(tdp['minCount']) >= int(p['minCount'])
+                        if p['maxCount'] != '*':
+                            assert int(tdp['maxCount']) <= int(p['maxCount'])
                     td['Properties'][k] = p
-        if 'Entries' in td:
-            pass    # process enumerated types
     return td
 
 
@@ -72,7 +75,7 @@ def make_types(model: str = MODEL_SNAPSHOT, out: str = OUT_DIR) -> None:
 
     # Update model_types to full type definitions after subclassing
     refs = defaultdict(list)
-    [refs[subclass(t)].append(t['Metadata']['name']) for t in model_types.values()]
+    [refs[subclass(t)].append(t['Metadata']['name']) for k, t in model_types.items() if not k.startswith('_')]
     [build_td(t, model_types) for t in reversed([e for e in TopologicalSorter(refs).static_order()])]
 
     print('Subclass tree:')
@@ -80,8 +83,8 @@ def make_types(model: str = MODEL_SNAPSHOT, out: str = OUT_DIR) -> None:
         print(f'{k:>30}: [{", ".join(v)}]')
 
     # Write class files for each type definition
-    for n, td in enumerate(model_types):
-        if td:
+    for td in model_types:
+        if not td.startswith('_'):
             write_tools_class(td, model_types, out)
 
 
